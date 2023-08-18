643d66fcda20005c6728ed76938ca9b983dbc6ec
Keiji O <meekunn@gmail.com>
2023-08-18 00:21:28 -0700

    cleaned some code

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,7 +12,6 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
   const contentDB = await openDB("jate", 1);
 
@@ -25,7 +24,6 @@ export const putDb = async (content) => {
   console.log("data saved to database", result);
 };
 
-// TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
   const contentDB = await openDB("jate", 1);
 
